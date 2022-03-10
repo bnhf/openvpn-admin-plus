6eb371db94fd829a14794189618828bfc01a08cb
Scott Ueland <41088895+bnhf@users.noreply.github.com>
2022-03-09 18:42:24 -0700

    Added alternate build commands for multi-arch

--- build/build.sh
+++ build/build.sh
@@ -7,5 +7,7 @@ PKGFILE=pivpn-tap-web-ui.tar.gz
 cp -f ../$PKGFILE ./
 
 docker build -t bnhf/pivpn-tap-web-ui:manifest-amd64 --build-arg ARCH=amd64/ .
+# docker build -t bnhf/pivpn-tap-web-ui:manifest-arm64 --build-arg ARCH=arm64/ .
+# docker build -t bnhf/pivpn-tap-web-ui:manifest-armv7 --build-arg ARCH=armv7/ .
 
 rm -f $PKGFILE
