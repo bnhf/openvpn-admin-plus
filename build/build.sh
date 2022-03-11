7442a61031308af07c0cee73f62cb58e7ea85012
Scott Ueland <41088895+bnhf@users.noreply.github.com>
2022-03-10 22:41:42 -0700

    Changed multi-arch to buildx method

--- build/build.sh
+++ build/build.sh
@@ -6,8 +6,12 @@ PKGFILE=pivpn-tap-web-ui.tar.gz
 
 cp -f ../$PKGFILE ./
 
-docker build -t bnhf/pivpn-tap-web-ui:manifest-amd64 --build-arg ARCH=amd64/ .
+# Multi-arch the manifest way
+# docker build -t bnhf/pivpn-tap-web-ui:manifest-amd64 --build-arg ARCH=amd64/ .
 # docker build -t bnhf/pivpn-tap-web-ui:manifest-arm64 --build-arg ARCH=arm64/ .
 # docker build -t bnhf/pivpn-tap-web-ui:manifest-armv7 --build-arg ARCH=armv7/ .
+
+# Multi-arch the buildx way
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t bnhf/pivpn-tap-web-ui .
 
 rm -f $PKGFILE
