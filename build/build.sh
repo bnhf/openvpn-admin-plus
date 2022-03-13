a7fae48649afaf7e0282898afe8f052a19e5c64a
bnhf <41088895+bnhf@users.noreply.github.com>
2022-03-13 14:06:05 -0600

    Updated build files for multi-arch containers

--- build/build.sh
+++ build/build.sh
@@ -6,12 +6,12 @@ PKGFILE=pivpn-tap-web-ui.tar.gz
 
 cp -f ../$PKGFILE ./
 
-# Multi-arch the manifest way
+# Multi-arch the manifest way -- uncomment for each architecture, save and run script
 # docker build -t bnhf/pivpn-tap-web-ui:manifest-amd64 --build-arg ARCH=amd64/ .
 # docker build -t bnhf/pivpn-tap-web-ui:manifest-arm64 --build-arg ARCH=arm64/ .
 # docker build -t bnhf/pivpn-tap-web-ui:manifest-armv7 --build-arg ARCH=armv7/ .
 
-# Multi-arch the buildx way
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t bnhf/pivpn-tap-web-ui . --push
+# Multi-arch the buildx way -- just use the command below, don't run the script
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -f build/Multi-arch.dockerfile -t bnhf/pivpn-tap-web-ui . --push
 
 rm -f $PKGFILE
